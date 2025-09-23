e073022d1fe7286dee3e73ef24e73b7c810d5e17
Yaremchuk_Lyudmila <layaremchuk@dev.vtb.ru>
2025-09-24 02:01:00 +0300

    Задание. UI Тесты. Selenium WD

--- src/test/java/edu/innotech/PikabuTest.java
+++ src/test/java/edu/innotech/PikabuTest.java
@@ -1,9 +1,9 @@
 package edu.innotech;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -23,9 +23,28 @@ public class PikabuTest {
     }
 
     @Test
-    public void test() {
+    @Order(1)
+    public void open() {
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        webDriver.get("https://pobeda.aero/");
+        webDriver.get("https://pikabu.ru/");
+        Assertions.assertEquals(webDriver.getTitle(), "Горячее – самые интересные и обсуждаемые посты | Пикабу");
+    }
+
+    @Test
+    @Order(2)
+    public void enter() {
+        WebElement enterButton = webDriver.findElement(By.xpath("//div[@class='header-right-menu']/button"));
+        enterButton.click();
+        WebElement username = webDriver.findElement(By.xpath("//input[@name='username']"));
+        WebElement password = webDriver.findElement(By.xpath("//input[@name='password']"));
+        WebElement enterButton2 = webDriver.findElement(By.xpath("//button[@type='submit']"));
+
+        username.sendKeys("Qwerty");
+        password.sendKeys("Qwerty");
+        enterButton2.click();
+
+        WebElement errorMessage = webDriver.findElement(By.xpath("//span[text()='Ошибка. Вы ввели неверные данные авторизации']"));
+        Assertions.assertTrue(errorMessage.isDisplayed());
     }
 
 }
